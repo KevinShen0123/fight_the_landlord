33c8c1b1cf4cf82555ae8f8b492050f23393fe4c
SwordXiaoJ <408555810@qq.com>
2024-04-18 12:32:41 -0400

    update CICD windows

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('test player', async ({ page }) => {
+  await page.goto('http://localhost:8221/api/login?key=foo-bar-baz&user=qingli&role=Player');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('test admin', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
